bb5b6c0b5ab83773fb08903366554cf89096a167
khd <khd1323@naver.com>
2020-09-06 01:59:56 +0900

    4-6 출력 메모리 비트 스트림의 구현

--- multiplayer-game-book/MemoryBitStream.cpp
+++ multiplayer-game-book/MemoryBitStream.cpp
@@ -1,1 +1,48 @@
 #include "RoboCatPCH.h"
+
+void OutputMemoryBitStream::WriteBits(uint8_t inData, size_t inBitCount)
+{
+	uint32_t nextBitHead = mBitHead + static_cast<uint32_t>(inBitCount);
+	if (nextBitHead > mBitCapacity)
+	{
+		ReallocBuffer(std::max(mBitCapacity * 2, nextBitHead));
+	}
+	// byteOffset : 비트 헤드를 8로 나눔
+	// bitOffset : 8로 나눈 비트헤드의 나머지
+	uint32_t byteOffset = mBitHead >> 3;
+	uint32_t bitOffset = mBitHead & 0x7;
+
+	// 현재 처리 중 바이트에서 몇 비트를 남길지 계산
+	uint8_t currentMask = ~(0xFF << bitOffset);
+	mBuffer[byteOffset] = (mBuffer[byteOffset] & currentMask) | (inData << bitOffset);
+
+	// 기록할 바이트에 아직 몇 비트가 남아있나 계산
+	uint32_t bitsFreeThisByte = 8 - bitOffset;
+
+	// 비트가 모자라면 다음 바이트에 넘김
+	if (bitsFreeThisByte < inBitCount)
+	{
+		// 다음 바이트에 나머지 비트를 기록
+		mBuffer[byteOffset + 1] = inData >> bitsFreeThisByte;
+	}
+	
+	mBitHead = nextBitHead;
+}
+
+void OutputMemoryBitStream::WriteBits(const void* inData, size_t inBitCount)
+{
+	const char* srcByte = static_cast<const char*>(inData);
+
+	// 바이트를 하나씩 모두 기록
+	while (inBitCount > 8)
+	{
+		WriteBits(*srcByte, 8);
+		++srcByte;
+		inBitCount -= 8;
+	}
+	// 아직 남은 비트를 기록
+	if (inBitCount > 0)
+	{
+		WriteBits(*srcByte, inBitCount);
+	}
+}
