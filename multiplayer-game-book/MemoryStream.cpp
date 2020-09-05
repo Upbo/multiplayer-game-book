60565c6963acaba22ae4674e2207ad24ae1253c5
khd <khd1323@naver.com>
2020-09-06 00:41:23 +0900

    4-2 입력 메모리 스트림

--- multiplayer-game-book/MemoryStream.cpp
+++ multiplayer-game-book/MemoryStream.cpp
@@ -14,10 +14,25 @@ void OutputMemoryStream::Write(const void* inData, size_t inByteCount)
 	uint32_t resultHead = mHead + static_cast<uint32_t>(inByteCount);
 	if (resultHead > mCapacity)
 	{
+		// 데이터를 기록하는 도중 mBuffer의 용량이 모자랄 경우 확장
 		ReallocBuffer(std::max(mCapacity * 2, resultHead));
 	}
 	// 버퍼의 제일 앞에 복사
 	std::memcpy(mBuffer + mHead, inData, inByteCount);
 	// mHead를 전진시켜 다음 기록에 대비
+	mHead = resultHead;
+}
+
+void InputMemoryStream::Read(void* outData, uint32_t inByteCount)
+{
+	uint32_t resultHead = mHead + inByteCount;
+	if (resultHead > mCapacity)
+	{
+		//handle error, no data to read!
+		//...
+	}
+
+	std::memcpy(outData, mBuffer + mHead, inByteCount);
+
 	mHead = resultHead;
 }
